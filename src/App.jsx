aaadf58f936493bc6ca2b7b0657ca9ec3b92d7a0
Tanya Gupta <94123994+TANYA03GUPTA@users.noreply.github.com>
2024-11-02 22:00:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,9 @@ function App() {
               </>
             }
           />
+            {/*
           <Route path="/contact" element={<Contact />} />
-          {/*
+        
       <TopInfo />
       <Body />
           <Contact />*/}
